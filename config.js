e997ab0e1e827e08e119e1b603defa5d2ad6235a
Apriadi000 <103306472+Apriadi000@users.noreply.github.com>
2022-04-17 19:24:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 global.DeveloperMode = 'false' //true atau false, true berati iya false berati tidak Moga paham
-global.owner = ['628818770766'] // masuk in no lu sebagai owner
-global.mods = ['6283897390164'] // masuk in no moderator ny
+global.owner = ['6287703642390'] // masuk in no lu sebagai owner
+global.mods = ['6282210190420'] // masuk in no moderator ny
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json')) // ini buat premium
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -30,15 +30,15 @@ global.APIKeys = { // Apikey lu di sini
 }
 
 // Sticker Watermark
-global.packname = 'NayLaBotz'
-global.author = 'Zevano'
+global.packname = '＼⋆⃟K⋆⃟A𓆩ᵇᵒᵗ𓆪⋆⃟T ⋆⃟E／'
+global.author = 'KATE AR RAWAWIE'
 
 global.multiplier = 36 // ini butuh berapa xp buat levelup 
 
 global.wait = '*❒ WET TUNGGU*'
 global.error = '*❒ ERROR CUK MAAP*'
-global.botwm = '© nayla' // watermark bot
-global.oname = 'Zevano' // nama owner nya
+global.botwm = '© ＼⋆⃟K⋆⃟A𓆩ᵇᵒᵗ𓆪⋆⃟T ⋆⃟E／' // watermark bot
+global.oname = 'KATE AR RAWAWIE' // nama owner nya
 global.thum = 'https://telegra.ph/file/5859353a380d9b67be198.jpg'
 
 let chalk = require('chalk')
